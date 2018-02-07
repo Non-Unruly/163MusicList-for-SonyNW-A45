8d4bc20059c6ea242dc62babfc763aaee8a23d23
未狂 <shihoongbo@sina.com>
2018-02-07 16:44:12 +0800

    歌词下载导入
    1.获取歌单信息时，增加歌曲ID项目，以搜索歌词
    2.增加getMusicLyric、CreateLRC函数，根据歌曲ID获取网易云音乐的歌词文件，并生成lrc文件
    3.将分割文件名、后缀名功能独立为一个函数splitNameFormat

--- code/Netease163Manager.py
+++ code/Netease163Manager.py
@@ -5,167 +5,199 @@ import os.path
 import requests
 
 FileFormat = ['mp3', "wma", 'flac', 'wav', 'aac', 'dsd', 'ape', 'mqa', 'MP3', 'WMA', 'FLAC', 'WAV', 'AAC', 'DSD', 'APE',
-			  'MQA']
+              'MQA']
 MusicNameList = []
 ErrorList = []  # 未能成功匹配的歌曲信息[列表内的标号，歌曲名]
 
 
+# 分割文件名和后缀名
+def splitNameFormat(path):
+    slice = path.split('.')
+    num = len(slice)
+    name = '.'.join(slice[0:num - 1])
+    format = slice[-1]
+    return name, format
+    pass
+
+
 # 半角符号全部升级为全角
-def CharacterCodeUnify (data):
-	newData = ''
-	for ch in data:
-		if ch == u'/' or ch == u'(' or ch == u')' or ch == u':':
-			num = ord(ch) + 65248
-			ch = chr(num)
-		newData += ch
-	newData = newData.lower()  # 统一小写匹配
-	newData = ''.join(newData.split(' '))
-	return newData
+def CharacterCodeUnify(data):
+    newData = ''
+    for ch in data:
+        if ch == u'/' or ch == u'(' or ch == u')' or ch == u':':
+            num = ord(ch) + 65248
+            ch = chr(num)
+        newData += ch
+    newData = newData.lower()  # 统一小写匹配
+    newData = ''.join(newData.split(' '))
+    return newData
 
 
 # 比较播放列表中的音频信息与本地文件是否一致
-def compare (netInfo, localName):
-	song = CharacterCodeUnify(netInfo['Song'])
-	singer = CharacterCodeUnify(netInfo["Singer"])
-	if song in localName and singer in localName:
-		return True
-	else:
-		# data1 = " @@%s %s |" % (netInfo['Singer'], netInfo['Song'])
-		# data2 = "%s %s \n" % (singer, song)
-		# data3 = "%s || " % (localName)
-		# hFile = open("d:\\log.txt", "a+", encoding="UTF-8")
-		# hFile.write(data3)
-		# hFile.write(data1)
-		# hFile.write(data2)
-		# hFile.close()
-		return False
+def compare(netInfo, localName):
+    song = CharacterCodeUnify(netInfo['Song'])
+    singer = CharacterCodeUnify(netInfo["Singer"])
+    if song in localName and singer in localName:
+        return True
+    else:
+        # data1 = " @@%s %s |" % (netInfo['Singer'], netInfo['Song'])
+        # data2 = "%s %s \n" % (singer, song)
+        # data3 = "%s || " % (localName)
+        # hFile = open("d:\\log.txt", "a+", encoding="UTF-8")
+        # hFile.write(data3)
+        # hFile.write(data1)
+        # hFile.write(data2)
+        # hFile.close()
+        return False
 
 
 # 获取歌单详细信息
-def requestGetList (id):
-	url = "http://music.163.com/api/playlist/detail?id=" + id
-	headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-			   'Accept-Language': 'zh-CN,zh;q=0.8',
-			   'User-Agent': 'BM_Recluse , Python-requests',
-			   'Upgrade-Insecure-Requests': '1',
-			   'Connection': 'keep-alive',
-			   'Host': 'music.163.com'}
-	#    print("url: " + url)
-	data = requests.get(url, headers = headers)
-	print(data.status_code)
-	print(data.headers, end = "\n\n\n")
-	return data.text
+def requestGetList(id):
+    url = "http://music.163.com/api/playlist/detail?id=" + id
+    headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+               'Accept-Language': 'zh-CN,zh;q=0.8',
+               'User-Agent': 'BM_Recluse , Python-requests',
+               'Upgrade-Insecure-Requests': '1',
+               'Connection': 'keep-alive',
+               'Host': 'music.163.com'}
+    #    print("url: " + url)
+    data = requests.get(url, headers=headers)
+    print(data.status_code)
+    print(data.headers, end="\n\n\n")
+    return data.text
 
 
 # 获取歌单内的全部歌曲的本地文件名
-def GetMusicNameList (id):
-	# data=requestGetList(id)
-	data = requestGetList(id)
-	listJson = json.loads(data)
-	list = listJson['result']['tracks']
-	for item in list:
-		Song = item['name']
-		Singer = item['artists'][0]['name']
-		Singer = CharacterCodeUnify(Singer)
-		Song = CharacterCodeUnify(Song)
-		# print("Sone:%s  Singer:%s" % (Song, Singer))
-		# obj = "%s - %s" % (Singer, Song)
-		obj = {"Song": Song, "Singer": Singer}
-		MusicNameList.append(obj)
-	# print(len(MusicNameList))
-	# print(MusicNameList)
-	return MusicNameList
+def GetMusicNameList(id):
+    # data=requestGetList(id)
+    data = requestGetList(id)
+    listJson = json.loads(data)
+    list = listJson['result']['tracks']
+    for item in list:
+        Song = item['name']
+        Singer = item['artists'][0]['name']
+        Singer = CharacterCodeUnify(Singer)
+        Song = CharacterCodeUnify(Song)
+        SongID = item['id']
+        # print("Sone:%s  Singer:%s" % (Song, Singer))
+        # obj = "%s - %s" % (Singer, Song)
+        obj = {"Song": Song, "Singer": Singer, "id": SongID}
+        MusicNameList.append(obj)
+    # print(len(MusicNameList))
+    # print(MusicNameList)
+    return MusicNameList
 
 
 # 获取指定目录下存在歌单内的音频文件
-def GetExistsFileAbsPathList (path):
-	FileAbsPathList = []
-	for p in os.listdir(path):
-		thisPath = path + "\\" + p
-		if os.path.isfile(thisPath):
-			# 如果是文件
-			i = -1
-			format = ''
-			length = len(p)
-			while True:
-				if p[i] != '.':
-					format = p[i] + format
-					i = i - 1
-				else:
-					break
-				if (-i >= length):
-					break
-			name = p[0:len(p) - len(format)]
-			name = CharacterCodeUnify(name)
-			if format in FileFormat:
-				isMatching = False
-				num = len(MusicNameList)
-				No = 0
-				for No in range(num):
-					if compare(MusicNameList[No], name):
-						FileAbsPathList.append({"No": No, "Path": thisPath})  # 增加歌单顺序Key='No'
-						isMatching = True
-						break
-					else:
-						continue
-				'''
-				for iter in MusicNameList:
-					if compare(iter, name):
-						FileAbsPathList.append(thisPath)
-						isMatching = True
-						break
-					else:
-						continue
-					pass
-				'''
-				if not isMatching:
-					# print("  Error Name:" + p)
-					pass
-			else:
-				# print("   Error Format:" + p)
-				pass
-			pass
-		elif os.path.isdir(thisPath):
-			# 如果是目录
-			FileAbsPathList.extend(GetExistsFileAbsPathList(thisPath))
-			pass
-	return FileAbsPathList
+def GetExistsFileAbsPathList(path):
+    FileAbsPathList = []
+    for p in os.listdir(path):
+        thisPath = path + "\\" + p
+        if os.path.isfile(thisPath):
+            # 如果是文件
+            name, format = splitNameFormat(p)
+            # i = -1
+            # format = ''
+            # length = len(p)
+            # while True:
+            #     if p[i] != '.':
+            #         format = p[i] + format
+            #         i = i - 1
+            #     else:
+            #         break
+            #     if -i >= length:
+            #         break
+            # name = p[0:len(p) - len(format)]
+            name = CharacterCodeUnify(name)
+            if format in FileFormat:
+                isMatching = False
+                num = len(MusicNameList)
+                No = 0
+                for No in range(num):
+                    if compare(MusicNameList[No], name):
+                        FileAbsPathList.append(
+                            {"No": No, "Path": thisPath, "id": MusicNameList[No]['id']})  # 增加歌单顺序Key='No'
+                        isMatching = True
+                        break
+                    else:
+                        continue
+                if not isMatching:
+                    print("  Error Name:" + p)
+                    pass
+            else:
+                print("   Error Format:" + p)
+                pass
+            pass
+        elif os.path.isdir(thisPath):
+            # 如果是目录
+            FileAbsPathList.extend(GetExistsFileAbsPathList(thisPath))
+            pass
+    return FileAbsPathList
 
 
 # 根据云歌单顺序进行本地歌单重排序
-def SortAbsPathList (unorderedPathList):
-	length = len(MusicNameList)
-	orderedPathList = length * [None]
-	for iter in unorderedPathList:
-		No = iter['No']
-		orderedPathList[No] = iter
+def SortAbsPathList(unorderedPathList):
+    length = len(MusicNameList)
+    orderedPathList = length * [None]
+    for iter in unorderedPathList:
+        No = iter['No']
+        orderedPathList[No] = iter
 
-	# 获取未能成功匹配的歌曲列表
-	for i in range(0, length):
-		if orderedPathList[i] == None:
-			name = MusicNameList[i]['Singer'] + ' - ' + MusicNameList[i]['Song']
-			ErrorList.append({'No': i, 'Music': name})
+    # 获取未能成功匹配的歌曲列表
+    for i in range(0, length):
+        if orderedPathList[i] == None:
+            name = MusicNameList[i]['Singer'] + ' - ' + MusicNameList[i]['Song']
+            ErrorList.append({'No': i, 'Music': name})
 
-	return orderedPathList
+    return orderedPathList
 
 
 # 云歌单与本地音频文件进行匹配，返回匹配成功的音频地址列表和未匹配到的歌曲列表
 # DirPath本地音频存放目录
 # MusicListId云歌单Id
-def getMusicAbsPathList (DirPath, MusicListId):
-	MusicNameList = GetMusicNameList(MusicListId)
-	print("MusicNameList:")
-	for it in MusicNameList:
-		print(it)
-	unorderedPathList = GetExistsFileAbsPathList(DirPath)
-	print("ExistsPath:")
-	for it in unorderedPathList:
-		print(it)
-	orderedPathList = SortAbsPathList(unorderedPathList)
-	print(len(orderedPathList))
-	for it in orderedPathList:
-		print(it)
-	print("Error Music")
-	for it in ErrorList:
-		print(it)
-	return orderedPathList, ErrorList
+def getMusicAbsPathList(DirPath, MusicListId):
+    MusicNameList = GetMusicNameList(MusicListId)
+    print("MusicNameList:")
+    for it in MusicNameList:
+        print(it)
+    unorderedPathList = GetExistsFileAbsPathList(DirPath)
+    print("\nExistsPath:")
+    for it in unorderedPathList:
+        print(it)
+    orderedPathList = SortAbsPathList(unorderedPathList)
+    print("匹配成功列表 %d" % (len(orderedPathList)))
+    for it in orderedPathList:
+        print(it)
+    print("\nError Music %d" % (len(ErrorList)))
+    for it in ErrorList:
+        print(it)
+    print("\nError Music end\n")
+    return orderedPathList, ErrorList
+
+
+# 获取歌词lyric
+# 参数：歌曲id（网易云ID）
+# 参数：音频文件地址
+def getMusicLyric(id, path):
+    url = 'http://music.163.com/api/song/lyric?os=pc&id=' + id + '&lv=1'
+    headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+               'Accept-Encoding': 'gzip, deflate, sdch',
+               'Accept-Language': 'zh-CN,zh;q=0.8',
+               'User-Agent': 'BM_Recluse , Python-requests',
+               'Upgrade-Insecure-Requests': '1',
+               'Connection': 'keep-alive',
+               'Host': 'music.163.com'}
+    data = requests.get(url, headers=headers)
+    # print(data.text)
+    res = json.loads(data.text)
+    isNoLrc = res.get('nolyric', False)
+    if isNoLrc:
+        return False
+    lrc = res['lrc']['lyric']
+    name, _format = splitNameFormat(path)
+    lrcPath = name + '.lrc'
+    hFile = open(lrcPath, "w+", encoding='utf-8')
+    hFile.write(lrc)
+    hFile.close()
+    return True
+    pass
--- code/m3uManager.py
+++ code/m3uManager.py
@@ -1,27 +1,37 @@
 import Netease163Manager
 
 
-def CreateM3u_inside (m2uPath, playList):
-	hFile = open(m2uPath, 'w+', encoding = 'utf-8')
-	hFile.write('#EXTM3U\n')
-	lst = [None]
-	for iter in playList:
-		if iter == None:
-			continue
-		print('---' + iter['Path'])
-		data = iter['Path'].split('\\')
-		length = len(data)
-		path = ''
-		for i in range(2, length):
-			path += data[i]
-			if i<length-1:
-				path += '\\'
-		hFile.write('#EXTINF:,\n')
-		hFile.write(path)
-		hFile.write('\n')
-	hFile.close()
-	pass
+def CreateM3u_inside(m2uPath, playList):
+    hFile = open(m2uPath, 'w+', encoding='utf-8')
+    hFile.write('#EXTM3U\n')
+    lst = [None]
+    for iter in playList:
+        if iter == None:
+            continue
+        # print(iter['Path'])
+        data = iter['Path'].split('\\')
+        length = len(data)
+        path = ''
+        for i in range(2, length):
+            path += data[i]
+            if i < length - 1:
+                path += '\\'
+        hFile.write('#EXTINF:,\n')
+        hFile.write(path)
+        hFile.write('\n')
+    hFile.close()
+    pass
 
 
-# netList,ErrorList = Netease163Manager.getMusicAbsPathList('E:\\Music', '111007305')
-# CreateM3u_inside('d:\\list.m3u', netList)
+# 创建歌词文件
+def CreateLRC(netList):
+    for it in netList:
+        id = str(it['id'])
+        path = it['Path']
+        res = Netease163Manager.getMusicLyric(id, path)
+        # print('%s %s' % (path, str(res)))
+    pass
+
+
+netList, ErrorList = Netease163Manager.getMusicAbsPathList('H:\\MUSIC\\女声古风', '920755371')
+CreateLRC(netList)
