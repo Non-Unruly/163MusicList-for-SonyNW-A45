e726e25e42e863adb824533864b28e41128359f8
未狂 <shihoongbo@sina.com>
2018-05-04 17:11:03 +0800

    增加M3U播放列表编辑模块

---
+++ code/M3U_Editor.py
@@ -0,0 +1,26 @@
+def LoadM3U(path):
+    try:
+        path_item = path.split("\\")
+        path_item = path_item[0:-1]
+        first_path = "\\".join(path_item) + "\\"
+
+        m3uFile = open(path, "r", encoding="utf-8")
+        data = ""
+        for line in m3uFile.readlines():
+            data += line.replace(",", "").replace("\r", "").replace("\n", "").replace(":", "")
+        tmpLst = data.split("#EXTINF")
+
+        lst = []
+        for item in tmpLst:
+            if item == "#EXTM3U" or item == "":
+                continue
+            lst.append({"absolute": first_path + item, "relative": item})
+        return lst
+    except Exception as e:
+        print("function -LoadM3U- Error: " + str(e))
+        return False
+
+
+lst = LoadM3U("C:\\Users\\Administrator\\Desktop\\最近在听\\最近在听.m3u")
+for it in lst:
+    print(it)
